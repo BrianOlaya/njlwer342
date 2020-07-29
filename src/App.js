1266a659a3323146ce86fdc3e8c8f950ffb03ef2
brianOlaya <brilot777@gmail.com>
2020-07-29 15:07:07 -0600

    solved, pending correct text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 /// Modifica el componente para que se puedan agregar tareas
 
-class App extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="list">
-          <h3>Por hacer:</h3>
-          <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
-          </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
-        </div>
-      </div>
-    )
+const App = () => {
+
+  const [listTasks, updateTasks] = useState({
+    task: ''
+  })
+
+
+  const [allTasks, saveTask]=useState(['Sacar la ropa', 'Hacer la cama', 'Leer un rato'])
+
+
+  const handleChange = (e) => {
+    updateTasks({
+
+      [e.target.name]: e.target.value
+    })
   }
+
+
+  const addTask = task => {
+     saveTask([
+       ...allTasks,
+       task
+     ])
+  }
+
+  const { task } = listTasks;
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+   
+    addTask(task)
+
+    updateTasks({
+      task:''
+    })
+  }
+
+
+
+  return (
+    <div className="wrapper">
+      <div className="list">
+        <h3>Por hacer:</h3>
+        <ul className="todo">
+          {allTasks.map(task=>(
+              <li>{task}</li>
+            )) }
+        </ul>
+        <form
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="text"
+            name="task"
+            id="new-task"
+            placeholder="Ingresa una tarea y oprime Enter"
+            value={task}
+            onChange={handleChange}
+
+          />
+        </form>
+      </div>
+    </div>
+  )
+
 }
 
 
